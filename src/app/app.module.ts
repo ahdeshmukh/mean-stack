33074a7ea5e993264434f9e86bb1e667a9b27b0c
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-26 11:44:30 -0400

    removing header module from app module as it is not needed there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { AdHeaderModule } from './modules/ad-header/ad.header.module'
 import { AdLoginModule } from './modules/ad-login/ad.login.module'
 import { AdRegistrationModule } from './modules/ad-registration/ad.registration.module'
 
@@ -13,7 +12,6 @@ import { AdRegistrationModule } from './modules/ad-registration/ad.registration.
   ],
   imports: [
     BrowserModule,
-    AdHeaderModule,
     AdLoginModule,
     AdRegistrationModule
   ],

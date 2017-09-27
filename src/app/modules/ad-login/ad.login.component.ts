8902779f61e58d0600ab5f75c3a5c009a44da5a3
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-26 21:26:01 -0400

    reactive form beginning - keeping code safe - revert to previous if things dont look right

--- src/app/modules/ad-login/ad.login.component.ts
+++ src/app/modules/ad-login/ad.login.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { UtilityService } from '../../services/utility/utility.service';
 
@@ -14,11 +15,26 @@ export class AdLoginComponent {
   @Input() showRegisterLink: boolean;
   @Output() onShowingRegistrationForm = new EventEmitter<boolean>();
 
+  rForm: FormGroup;
+  post:any; // property for our submitted form
+  password:string = '';
+  email:string = '';
+
   showRegistrationForm(showRegistrationForm: boolean) {
     this.onShowingRegistrationForm.emit(showRegistrationForm);
   }
   
-  constructor(private _utilityService: UtilityService) {}
+  constructor(private _utilityService: UtilityService, private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'email' : [null, Validators.required],
+      'password' : [null, Validators.required]
+    });
+  }
+
+  addPost(post) {console.log(post);
+    this.password = post.password;
+    this.email = post.email;
+  }
   
   ngOnInit() {
     this.images_base_path = this._utilityService.getImagesBasePath();
--- src/app/modules/ad-login/ad.login.module.ts
+++ src/app/modules/ad-login/ad.login.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdLoginComponent } from './ad.login.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   declarations: [AdLoginComponent],
   exports: [AdLoginComponent]
 })

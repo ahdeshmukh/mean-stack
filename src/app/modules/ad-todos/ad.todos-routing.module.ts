3cc54e6c049205f6d2b00c14f0004b79f7273ba5
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-11-10 13:26:09 -0500

    basic auth guard, work in proress

--- src/app/modules/ad-todos/ad.todos-routing.module.ts
+++ src/app/modules/ad-todos/ad.todos-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule }             from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
  
-import { AdTodosComponent }    from './ad.todos.component';
+import { AdTodosComponent } from './ad.todos.component';
+import { AuthguardService } from '../../services/auth/authguard.service';
+import { UserService } from '../../services/user/user.service';
  
 const todosRoutes: Routes = [
-  { path: 'todos',  component: AdTodosComponent }
+  { path: 'todos',  component: AdTodosComponent, canActivate:[AuthguardService] }
 ];
  
 @NgModule({
@@ -13,6 +15,10 @@ const todosRoutes: Routes = [
   ],
   exports: [
     RouterModule
+  ],
+  providers: [
+    AuthguardService,
+    UserService
   ]
 })
 export class AdTodosRoutingModule { }

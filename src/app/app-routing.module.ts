04d7a479f4543d207095b617e27d73aa25654295
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-12-31 17:18:23 -0500

    handling unmatched routes - redirecting to todos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdHomeComponent } from './modules/ad-home/ad.home.component';
 
+// { path: '**', redirectTo: '/', pathMatch: 'full' } is to catch unmatched routes, otherwise app breaks
 const appRoutes: Routes = [
   { path: '', pathMatch: 'full', component: AdHomeComponent},
+  { path: '**', redirectTo: 'todos', pathMatch: 'full' }
 ];
 
 @NgModule({
